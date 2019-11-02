0d350753fcc3433770d42b3c3f46be833de926fa
Maserall Marcelin <maserallm@knights.ucf.edu>
2019-11-01 22:08:36 -0400

    moment js times and statements shown on html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,36 +11,63 @@ $("#add-train").on("click", function (event) {
     event.preventDefault();
 
     let trainName = $("#train-name").val().trim();
-    let desti = $("#destination").val().trim();
+    let destination = $("#destination").val().trim();
     let firstTrainTime = $("#t-time").val().trim();
-    let freq = $("#frequency").val().trim();
+    let frequency = $("#frequency").val().trim();
 
     database.ref().push({
         train: trainName,
-        destination: desti,
+        destination: destination,
         arrival: firstTrainTime,
-        frequency: freq,
+        frequency: frequency,
     });
-    console.log(event);
-})
+    
+}); //submit button
 
-database.ref().on("child_added", function(snapshot){
+
+database.ref().on("child_added", function (snapshot) {
     let snapVal = snapshot.val();
 
     let trainName = snapVal.train;
     let destination = snapVal.destination;
-    let nextArrival = snapVal.arrival;
+    let firstTrainTime = snapVal.arrival;
     let frequency = snapVal.frequency;
+
+    let timeDiff = moment().diff(moment(firstTrainTime, "HH:mm"), "minutes");
+    console.log(timeDiff);
+
+    let remaind = timeDiff % frequency;
+    console.log(remaind);
+
+    let minAway = frequency - remaind;
+    console.log(minAway);
+
+    let nextTrain = moment().add(minAway, "minutes").format("hh:mm A"); 
+    console.log(nextTrain);
 
     let trainAdded = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
-        $("<td>").text(nextArrival),
+        $("<td>").text(firstTrainTime),
         $("<td>").text(frequency),
+        $("<td>").text(minAway),
+        $("<td>").text(nextTrain),
     );
 
     $("#train-info").append(trainAdded);
 
+    
+    // let normalTime = moment(nextArrival, "HH:mm").format("hh:mm a");
+    //     console.log(normalTime);
+    // let minAway = moment().diff(normalTime, "minutes");
+    // console.log(minAway);
+    // let normalArrivalTime = moment(nextArrival, "HH:mm A").format("HH:mm A");
+    // //     console.log(normalArrivalTime);
+    // let distanceAway = moment().diff(moment(nextArrival, "HH:mm"), "minutes");
+    // console.log(distanceAway);
+});
 
-})
+// console.log(moment().format("HH:mm"));
+
+
 
